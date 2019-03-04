3fde1dc62c1063428bae24f3095967170363c31d
kvguarin <kvguarin@dons.usfca.edu>
2019-03-04 12:28:39 -0800

    starting chapter 9

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -4,9 +4,29 @@ import epi.test_framework.GenericTest;
 public class IsTreeSymmetric {
   @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
+  // TODO - you fill in here.
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if(tree == null){
+      return true;
+    }
+    return isSymmetric(tree.left, tree.right);
+  }
+
+  public static boolean isSymmetric(BinaryTreeNode<Integer> left, BinaryTreeNode<Integer> right){
+    if(left == null && right == null){
+      return true;
+    }
+    if(left == null || right == null){
+      return false;
+    }
+
+    if(left.data != right.data){
+      return false;
+    }
+    boolean sym1 = isSymmetric(left.left, right.right);
+    boolean sym2 = isSymmetric(left.right, right.left);
+
+    return sym1 && sym2;
   }
 
   public static void main(String[] args) {
